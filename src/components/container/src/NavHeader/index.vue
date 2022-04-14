fad1dfdea87e1bd2f17933cfa24732613d59d824
zmplly <1776321381@qq.com>
2022-04-14 23:37:51 +0800

    feat: 菜单栏抽离组件

---
+++ src/components/container/src/NavHeader/index.vue
@@ -0,0 +1,28 @@
+<!--  -->
+<template>
+  <el-header class="header">
+    <span @click="toggle">
+      <el-icon-expand v-if="collapse" />
+      <el-icon-fold v-else />
+    </span>
+  </el-header>
+</template>
+
+<script lang="ts" setup>
+  let props = defineProps<{
+    collapse: boolean;
+  }>();
+  let emits = defineEmits(["update:collapse"]);
+  const toggle = () => {
+    emits("update:collapse", !props.collapse);
+  };
+</script>
+<style lang="scss" scoped>
+
+  .header {
+    height: 60px;
+    padding: 0 20px !important;
+    display: flex;
+    align-items: center;
+  }
+</style>
--- src/components/container/src/index.vue
+++ src/components/container/src/index.vue
@@ -2,36 +2,10 @@
 <template>
   <el-container>
     <el-aside width="auto">
-      <el-menu
-        :collapse="isCollapse"
-        default-active="2"
-        class="el-menu-vertical-demo"
-      >
-        <el-menu-item index="1">
-          <el-icon-menu />
-          <span>导航1</span>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <el-icon-menu />
-          <span>导航2</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <el-icon-menu />
-          <span>导航3</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon-menu />
-          <span>导航4</span>
-        </el-menu-item>
-      </el-menu>
+      <nav-side :collapse="isCollapse"></nav-side>
     </el-aside>
     <el-container>
-      <el-header>
-        <span @click="toggle">
-          <el-icon-expand v-if="isCollapse" />
-          <el-icon-fold v-else />
-        </span>
-      </el-header>
+      <nav-header v-model:collapse="isCollapse"></nav-header>
       <el-main>
         <router-view></router-view>
       </el-main>
@@ -41,16 +15,14 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-
-let isCollapse = ref(false);
-
-const toggle = () => {
-  isCollapse.value = !isCollapse.value;
-};
+  import { ref } from "vue";
+  import NavHeader from "./NavHeader/index.vue";
+  import NavSide from "./NavSide/index.vue";
+  let isCollapse = ref(false);
 </script>
 <style lang="scss" scoped>
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-}
+  .el-header {
+    padding: 0;
+    border: 1px solid #eee;
+  }
 </style>
