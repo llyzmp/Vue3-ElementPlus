e6fec3fb75a16a5e620255bfbeb6f9dc53a919cc
zmplly <1776321381@qq.com>
2022-04-14 23:37:37 +0800

    feat: 菜单栏抽离组件

---
+++ src/components/container/src/NavSide/index.vue
@@ -0,0 +1,36 @@
+<!--  -->
+<template>
+  <el-menu
+    :collapse="collapse"
+    default-active="2"
+    class="el-menu-vertical-demo"
+  >
+    <el-menu-item index="1">
+      <el-icon-menu />
+      <span>首页</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon-menu />
+      <span>图标选择器</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon-menu />
+      <span>级联选择器</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon-menu />
+      <span>导航4</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+let props = defineProps<{
+  collapse: boolean,
+}>()
+
+</script>
+<style lang="scss" scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+}</style>
